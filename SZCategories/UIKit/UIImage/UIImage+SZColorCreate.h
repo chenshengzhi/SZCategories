213415957ad0246f772a626ef194f8f6dab3b85c
chenshengzhi <329012084@qq.com>
2016-08-24 11:53:56 +0800

    1.3.3

--- SZCategories/UIKit/UIImage/UIImage+SZColorCreate.h
+++ SZCategories/UIKit/UIImage/UIImage+SZColorCreate.h
@@ -12,12 +12,30 @@
 
 + (UIImage *)sz_imageWithColor:(UIColor *)color;
 
-+ (UIImage *)sz_imageWithColor:(UIColor *)color size:(CGSize)size;
++ (UIImage *)sz_imageWithColor:(UIColor *)color
+                          size:(CGSize)size;
 
-+ (UIImage *)sz_imageWithColor:(UIColor *)color size:(CGSize)size cornerRadius:(CGFloat)cornerRadius;
++ (UIImage *)sz_imageWithColor:(UIColor *)color
+                          size:(CGSize)size
+                  cornerRadius:(CGFloat)cornerRadius;
 
 
-+ (UIImage *)sz_gradientImageWithBounds:(CGRect)bounds colors:(NSArray *)colors startPoint:(CGPoint)startPoint endPoint:(CGPoint)endPoint;
++ (UIImage *)sz_imageWithColor:(UIColor *)color
+                          size:(CGSize)size
+                   borderColor:(UIColor *)borderColor
+                   borderWidth:(CGFloat)borderWidth;
+
++ (UIImage *)sz_imageWithColor:(UIColor *)color
+                          size:(CGSize)size
+                   borderColor:(UIColor *)borderColor
+                   borderWidth:(CGFloat)borderWidth
+                  cornerRadius:(CGFloat)cornerRadius;
+
+
++ (UIImage *)sz_gradientImageWithBounds:(CGRect)bounds
+                                 colors:(NSArray *)colors
+                             startPoint:(CGPoint)startPoint
+                               endPoint:(CGPoint)endPoint;
 
 - (UIImage *)sz_croppedImageWithRect:(CGRect)rect;
 
--- SZCategories/UIKit/UIImage/UIImage+SZColorCreate.m
+++ SZCategories/UIKit/UIImage/UIImage+SZColorCreate.m
@@ -14,14 +14,20 @@
     return [UIImage sz_imageWithColor:color size:CGSizeMake(1, 1)];
 }
 
-+ (UIImage *)sz_imageWithColor:(UIColor *)color size:(CGSize)size {
-    return [UIImage sz_imageWithColor:color size:size cornerRadius:0];
++ (UIImage *)sz_imageWithColor:(UIColor *)color
+                          size:(CGSize)size {
+    return [UIImage sz_imageWithColor:color
+                                 size:size
+                         cornerRadius:0];
 }
 
-+ (UIImage *)sz_imageWithColor:(UIColor *)color size:(CGSize)size cornerRadius:(CGFloat)cornerRadius {
++ (UIImage *)sz_imageWithColor:(UIColor *)color
+                          size:(CGSize)size
+                  cornerRadius:(CGFloat)cornerRadius {
     UIGraphicsBeginImageContextWithOptions(size, NO, [UIScreen mainScreen].scale);
     CGContextRef context = UIGraphicsGetCurrentContext();
-    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, size.width, size.height) cornerRadius:cornerRadius];
+    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, size.width, size.height)
+                                                    cornerRadius:cornerRadius];
     CGContextAddPath(context, path.CGPath);
     CGContextSetFillColorWithColor(context, color.CGColor);
     CGContextFillPath(context);
@@ -30,21 +36,66 @@
     return image;
 }
 
-+ (UIImage *)sz_gradientImageWithBounds:(CGRect)bounds colors:(NSArray *)colors startPoint:(CGPoint)startPoint endPoint:(CGPoint)endPoint {
+
++ (UIImage *)sz_imageWithColor:(UIColor *)color
+                          size:(CGSize)size
+                   borderColor:(UIColor *)borderColor
+                   borderWidth:(CGFloat)borderWidth {
+    return [UIImage sz_imageWithColor:color
+                                 size:size
+                          borderColor:borderColor
+                          borderWidth:borderWidth
+                         cornerRadius:0];
+}
+
++ (UIImage *)sz_imageWithColor:(UIColor *)color
+                          size:(CGSize)size
+                   borderColor:(UIColor *)borderColor
+                   borderWidth:(CGFloat)borderWidth
+                  cornerRadius:(CGFloat)cornerRadius {
+    UIGraphicsBeginImageContextWithOptions(size, NO, [UIScreen mainScreen].scale);
+
+    UIBezierPath *borderPath = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, size.width, size.height)
+                                                          cornerRadius:cornerRadius];
+    [borderColor setFill];
+    [borderPath fill];
+
+    UIBezierPath *mainPath = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(borderWidth,
+                                                                                borderWidth,
+                                                                                size.width-2*borderWidth,
+                                                                                size.height-2*borderWidth)
+                                                        cornerRadius:cornerRadius];
+    [color setFill];
+    [mainPath fill];
+
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+
++ (UIImage *)sz_gradientImageWithBounds:(CGRect)bounds
+                                 colors:(NSArray *)colors
+                             startPoint:(CGPoint)startPoint
+                               endPoint:(CGPoint)endPoint {
     UIGraphicsBeginImageContextWithOptions(bounds.size, NO, [UIScreen mainScreen].scale);
-    
+
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSaveGState(context);
-    
+
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
     NSMutableArray *cgColorArray = [NSMutableArray array];
     for(UIColor *c in colors) [cgColorArray addObject:(id)c.CGColor];
     CGGradientRef gradient = CGGradientCreateWithColors(colorSpace, (CFArrayRef)cgColorArray, NULL);
-    
-    CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, kCGGradientDrawsBeforeStartLocation | kCGGradientDrawsAfterEndLocation);
-    
+
+    CGContextDrawLinearGradient(context,
+                                gradient,
+                                startPoint,
+                                endPoint,
+                                kCGGradientDrawsBeforeStartLocation | kCGGradientDrawsAfterEndLocation);
+
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    
+
     CGGradientRelease(gradient);
     CGContextRestoreGState(context);
     CGColorSpaceRelease(colorSpace);
@@ -53,9 +104,14 @@
 }
 
 - (UIImage *)sz_croppedImageWithRect:(CGRect)rect {
-    CGRect scaleRect = CGRectMake(self.scale*rect.origin.x, self.scale*rect.origin.y, self.scale*rect.size.width, self.scale*rect.size.height);
+    CGRect scaleRect = CGRectMake(self.scale*rect.origin.x,
+                                  self.scale*rect.origin.y,
+                                  self.scale*rect.size.width,
+                                  self.scale*rect.size.height);
     CGImageRef croppedImageRef = CGImageCreateWithImageInRect(self.CGImage, scaleRect);
-    UIImage *croppedImage = [[UIImage alloc] initWithCGImage:croppedImageRef scale:self.scale orientation:self.imageOrientation];
+    UIImage *croppedImage = [[UIImage alloc] initWithCGImage:croppedImageRef
+                                                       scale:self.scale
+                                                 orientation:self.imageOrientation];
     CGImageRelease(croppedImageRef);
     return croppedImage;
 }
